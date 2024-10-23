cb63313e96749ac27094bfe7bedcf160184b283d
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-23 10:25:00 +0700

    create function layout

--- app/src/main/java/com/example/tataletak/layout.kt
+++ app/src/main/java/com/example/tataletak/layout.kt
@@ -1,2 +1,14 @@
 package com.example.tataletak
 
+import android.text.Layout
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun Layout(modifier: Modifier = Modifier){
+
+}
