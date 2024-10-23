056d028abae1ab9f327fe0ef2cf83bba8ee3d068
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-23 10:57:41 +0700

    add component: text

--- app/src/main/java/com/example/tataletak/layout.kt
+++ app/src/main/java/com/example/tataletak/layout.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true)
 @Composable
@@ -41,6 +44,11 @@ fun TampilanHeader(){
                     contentDescription = null,
                     modifier = Modifier .size(20.dp),
                     tint = Color.Red,)
+            }
+            Column (modifier = Modifier.padding(16.dp)){ 
+                Text(text = "Teknologi Informasi",
+                    color = Color.White,
+                    fontSize = 20.sp)
             }
         }
     }
