3a2d1b82d8df720fa6e70394c8fc02f62a0f5ede
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-23 10:41:19 +0700

    add box in row

--- app/src/main/java/com/example/tataletak/layout.kt
+++ app/src/main/java/com/example/tataletak/layout.kt
@@ -1,12 +1,15 @@
 package com.example.tataletak
 
 import android.text.Layout
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +26,10 @@ fun Layout(modifier: Modifier = Modifier){
 fun TampilanHeader(){
     Box(modifier = Modifier.fillMaxWidth().
         background(color = Color.DarkGray)){
+        Row {
+            Box(contentAlignment = Alignment.BottomEnd){
 
+            }
+        }
     }
 }
